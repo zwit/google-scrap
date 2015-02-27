b99034dd475dc441ea578f82086cb16e86e25074
zwit <tagada1992>
2015-02-27 08:48:23 +0100

    ajout UI

--- mygooglescrap/app/Http/Controllers/SearchController.php
+++ mygooglescrap/app/Http/Controllers/SearchController.php
@@ -5,6 +5,8 @@ use Response;
 use View;
 use Illuminate\Http\Request;
 
+require public_path().'/php/casperphpwrapper.php';
+
 class SearchController extends Controller {
 
 	public function __construct()
@@ -22,8 +24,35 @@ class SearchController extends Controller {
 		$this->validate($request, [
 			'search' => 'required',
 		]);
-		//return Response::make('Search done !');
-		return view('search');
+
+		putenv('PATH=C:/phantomjs;');
+		$forge = 'js\casperjs\batchbin\casperjs.bat js\casperjs\tests\commands\mytest.js "'.$request->input('search').'"';
+		if ($request->input('search') != '')
+			$forge .= ' '.($request->input('page')-1)*10;
+		exec($forge, $output);
+
+		//var_dump($output);
+
+		/*$casper = new \Browser\Casper();
+		$casper->setPath(public_path().'/php');
+		$casper->start("http://google.fr/");
+		$casper->fillForm(
+			'form[action="/search"]',
+			array(
+				'q' => 'search'
+			),
+			true);
+
+		var_dump($casper->run());
+		var_dump($casper->getRequestedUrls());
+		//var_dump($casper->getOutput());*/
+
+		//var_dump($output);
+		//var_dump(json_decode(implode($output)));
+
+		//return View::make('search', array('results' => json_decode(implode($output))));
+
+		return Response::json( (implode($output)) );
 	}
 
 }
--- mygooglescrap/resources/views/search.blade.php
+++ mygooglescrap/resources/views/search.blade.php
@@ -3,6 +3,9 @@
 	<title>Laravel</title>
 
 	<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+	{!! HTML::style('css/bootstrap.min.css'); !!}
+
+	{!! HTML::script('js/jquery-2.1.3.min.js'); !!}
 
 	<style>
 		body {
@@ -10,10 +13,10 @@
 			padding: 0;
 			width: 100%;
 			height: 100%;
-			color: #B0BEC5;
 			display: table;
-			font-weight: 100;
-			font-family: 'Lato';
+			background-color: #F0F0F0;
+			color: black;
+			font-family: sans-serif;
 		}
 
 		.container {
@@ -28,6 +31,38 @@
 			width:	100%;
 		}
 
+		#results {
+			text-align: left;
+			margin-top: 30px;
+			padding-left: 30px;
+		}
+
+		.rowres {
+			float:left;
+			clear: both;
+			margin-top: 10px;
+		}
+
+		.rowtitle {
+			float:left;
+			clear: both;
+			color: #1a0dab;
+			font-size: 18px;
+		}
+
+		.rowhref {
+			float:left;
+			clear: both;
+			color: #006621;
+			font-size: 14px;
+		}
+
+		.rowdesc {
+			float:left;
+			clear: both;
+			font-size: 12px;
+		}
+
 		.title {
 			font-size: 96px;
 			margin-bottom: 40px;
@@ -38,23 +73,109 @@
 		}
 	</style>
 
-	{!! HTML::style('css/bootstrap.min.css'); !!}
+	<script>
+		jQuery( document ).ready( function( $ ) {
+
+			var page;
+
+			function generateData(data) {
+				$('#results').html('');
+
+				tmp = JSON.parse(data);
+				$.each(tmp , function( index, obj ) {
+					$.each(obj, function( index2, value ) {
+						$('#results').append('<div class="rowres">');
+						$('#results').append('<div class="rowtitle">' + value["rowtitle"] + '</div>');
+						$('#results').append('<div class="rowhref">' + value["rowhref"] + '</div>');
+						$('#results').append('<div class="rowdesc">' + value["rowdesc"] + '</div>');
+						$('#results').append('</div>');
+					});
+				});
+			}
+
+			function sendData(nbpage) {
+				$('#ajax-loader').show();
+
+				page = nbpage;
+
+				$.post(
+						$( '#form-search' ).prop( 'action' ),
+						{
+							"_token": $( '#form-search' ).find( 'input[name=_token]' ).val(),
+							"search": $( '#search' ).val(),
+							"page": page
+						},
+						function( data ) {
+							generateData(data);
+
+							$('#ajax-loader').hide();
+						},
+						'json'
+				);
+			}
+
+			$( '#form-search' ).on( 'submit', function() {
+				sendData(1);
+
+				return false;
+			} );
+
+			$( '#first' ).on( 'click', function() {
+				sendData(1);
+			} );
+
+			$( '#prev' ).on( 'click', function() {
+				if (page > 1)
+					sendData(page - 1);
+			} );
+
+			$( '#next' ).on( 'click', function() {
+				sendData(page + 1);
+			} );
+
+		} );
+	</script>
 
 </head>
 <body>
 <div class="container">
 	<div class="content">
-		{!!  Form::open(array('action' => 'SearchController@scrap')) !!}
+		{!!  Form::open(array('action' => 'SearchController@scrap', 'id' => 'form-search')) !!}
 		<div class="row">
 			<div class="col-md-3">
-		{!!  Form::text('search','',array('id'=>'','class'=>'','placeholder'=>'Enter something bright','class'=>'form-control')) !!}
+		{!!  Form::text('search','',array('id'=>'search','class'=>'','placeholder'=>'Enter something bright','class'=>'form-control')) !!}
 			</div>
 			<div class="col-md-1">
 		{!!  Form::submit('Search', array('class'=>'btn btn-info')) !!}
 			</div>
+			<div class="col-md-1" id="ajax-loader" style="display:none">
+				{!! HTML::image('img/ajax-loader.gif') !!}
+			</div>
 		</div>
 		{!!  Form::close() !!}
+		<div class="row">
+			<div class="col-md-3">
+				{!! Form::button('Première Page', array('class'=>'btn btn-info', 'id'=>'first')) !!}
+			</div>
+			<div class="col-md-3">
+				{!! Form::button('Page Précédente', array('class'=>'btn btn-info', 'id'=>'prev')) !!}
+			</div>
+			<div class="col-md-3">
+				{!! Form::button('Page Suivante', array('class'=>'btn btn-info', 'id'=>'next')) !!}
+			</div>
+		</div>
+		<div id="results">
+			<?php if (isset($results)) {
+				//$results = json_decode(json_encode($results), true);
+				foreach ($results->{'results'} as $k => $v) {
+					echo '<br/><br/><br/>title : '.$v->{'title'};
+					echo '<br/>href : '.$v->{'href'};
+					echo '<br/>description : '.$v->{'desc'};
+				}
+			} ?>
+		</div>
 	</div>
+
 </div>
 </body>
 </html>
